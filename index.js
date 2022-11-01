7b85f7e6e4a6a6a0906dcbf1627691fe3dff27f9
brscn2 <brscn55@yahoo.com>
2022-11-01 14:37:40 +0300

    Database added!

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const app = express();
 const path = require('path');
 const methodOverride = require('method-override');
 const { v4: uuidv4 } = require('uuid');
-// const PORT = process.env.PORT || 8080
-const PORT = 3000;
+const PORT = process.env.PORT || 8080
 
 const { MongoClient } = require("mongodb");
 
